74460f01c2a597dc38b80909816a2a930cd82582
Bar <barmlaw01@gmail.com>
2023-08-09 20:06:04 +0300

    Removed Everything

--- infrastructure/Jenkins/helm.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p vic-jenkins-helm
-helm repo add jenkins https://charts.jenkins.io
-helm repo update
-helm pull jenkins/jenkins -d vic-jenkins-helm/charts
-helm package vic-jenkins-helm/ -d vic-jenkins-helm/
-docker login 
-helm push registry-1.docker.io/itsvictorfy/jenkins:v0.1
